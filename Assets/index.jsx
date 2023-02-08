d8cc043e66afb367c2473120e7a8733582805c8c
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-02-08 10:50:56 +0530

    quick error fix

---
+++ Assets/index.jsx
@@ -0,0 +1,11 @@
+import First from './First.png';
+import Second from './Second.png';
+import Third from './Third.png';
+import Fourth from './Fourth.png';
+
+export {
+    First,
+    Second,
+    Third,
+    Fourth
+}
